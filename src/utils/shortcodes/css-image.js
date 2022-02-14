a52d8d70fd251ec31bdae215ab1c30717f249a5f
Corey Keller <corey.james.keller@gmail.com>
2022-02-14 23:53:13 +0700

    mobile nav progress, button reset mixin

--- src/utils/shortcodes/css-image.js
+++ src/utils/shortcodes/css-image.js
@@ -6,13 +6,9 @@ function generateImages(src, widths){
   let source = `./src/images/${src}`;
   let options = {
     widths: widths,
-    formats: ['webp', 'jpeg'],
+    formats: ['jpeg'],
     outputDir: "./dist/img/",
     useCache: true
-    // sharpJpegOptions: {
-    //   quality: 99,
-    //   progressive: true
-    // }
   };
   // genrate images, ! dont wait
   Image(`./src/images/${src}`, options);
@@ -29,12 +25,6 @@ exports.imageCssBackground = (src, selector, widths) =>{
   metadata.jpeg.slice(1).forEach((image, idx) => {
     markup.push(`@media (min-width: ${metadata.jpeg[idx].width}px) { ${selector} {background-image: url(${image.url});}}`);
   });
-
-  // markup.push(`${selector} { background-image: url(${metadata.webp[0].url});} `);
-
-  // metadata.webp.slice(1).forEach((image, idx) => {
-  //   markup.push(`@media (min-width: ${metadata.webp[idx].width}px) { ${selector} {background-image: url(${image.url});}}`);
-  // });
 
   return markup.join("");
 }
